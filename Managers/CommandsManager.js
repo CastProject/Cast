241b9f78ad3511ab1712e7fd78eb348d24c9c566
Eric Rabil <EricRabil@users.noreply.github.com>
2017-06-21 22:53:59 -0400

    Made commands manager more efficient and error-handling

--- Managers/CommandsManager.js
+++ Managers/CommandsManager.js
@@ -16,43 +16,60 @@ class CommandsManager {
         this.plugin = plugin;
         this.commands = new Map();
         this.aliases = new Map();
-        this.loadCommands()
+        this.groups = new Map();
     }
 
-    load(file, settings) {
+    load(file, group, settings) {
         try {
             var Command = require(file);
-            var loadedCommand = new Command(this.client);
+            var loadedCommand = new Command(this.client, settings);
             if (!loadedCommand instanceof BaseCommand) {
                 this.client.log(`The ${settings.file} command from the ${settings.plugin} plugin could not be loaded because it is invalid.`, true)
             } else {
-                var command = settings.command ? settings.command : settings.file
+                var command = settings.command ? settings.command : settings.file;
                 var alias = settings.aliases ? settings.aliases : []
-                var args = settings.args
-                this.commands.set(command, {execute: loadedCommand.execute, args: args});
+                this.commands.set(command, loadedCommand);
                 alias.forEach(a => {
                     this.aliases.set(a, command)
-                })
+                });
+                this.groups.get(group).push(command);
             }
         } catch (e) {
-
+            if (e.message.includes('Cannot find module')) {
+                this.invalidMeta(settings, true);
+            } else this.client.logError(e);
         }
     }
 
+    get(cstr) {
+        return new Promise((resolve, reject) => {
+            var alias = this.aliases.get(cstr);
+            resolve(alias ? this.commands.get(alias) : this.commands.get(cstr));
+        });
+    }
+
     loadCommands() {
-        fs.exists(path.join(this.commandsPath, 'commands.json'), (exists) => {
-            if (!exists) {
-                this.client.log(`No commands.json found in ${path}! Commands will not be loaded`, true)
-                return;
-            }
-            var commandsMeta = require(path.join(this.commandsPath, 'commands.json'));
-            Object.keys(commandsMeta.categories).forEach(c => {
-                commandsMeta.categories[c].forEach(command => {
-                    if (!command.file) return;
-                    this.load(path.join(this.commandsPath, command.file), command)
+        return new Promise((resolve, reject) => {
+            fs.exists(path.join(this.commandsPath, 'commands.json'), (exists) => {
+                if (!exists) {
+                    this.client.log(`No commands.json found in ${path}! Commands will not be loaded`, true)
+                    return;
+                }
+                var commandsMeta = require(path.join(this.commandsPath, 'commands.json'));
+                Object.keys(commandsMeta.categories).forEach(c => {
+                    this.groups.set(c, [])
+                    commandsMeta.categories[c].forEach(command => {
+                        if (!command.file) return this.invalidMeta(command);
+                        this.load(path.join(this.commandsPath, command.file), c, command)
+                    })
                 })
+                resolve()
             })
         })
+    }
+
+    invalidMeta(availableMeta, missing = false) {
+        this.client.log(`${availableMeta.plugin ? `The ${availableMeta.plugin} plugin` : "A plugin"} tried to load command '${availableMeta.file ? availableMeta.file : availableMeta.command ? availableMeta.command : "a command"}' ${missing ? "but it could not be found" : "with invalid syntax"}!`, true)
     }
 }
 
