f0ddb1ac4c785351047362a524f31b7b9373357a
Eric Rabil <EricRabil@users.noreply.github.com>
2017-06-22 10:54:51 -0400

    Skeleton BasePlugin

---
+++ Objects/Plugin.js
@@ -0,0 +1,10 @@
+const EventEmitter = require('events').EventEmitter;
+const Events = require('discord.js').Constants.Events;
+
+class Plugin extends EventEmitter {
+    constructor(client, metadata) {
+        super();
+        this.client = client;
+        this.metadata = metadata;
+    }
+}
