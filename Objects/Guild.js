b7a62e96d65b0ed4181ef0396158de179d1d8ab9
Eric Rabil <EricRabil@users.noreply.github.com>
2017-06-23 10:00:31 -0400

    MessagesManager is now client-independent

--- Objects/Guild.js
+++ Objects/Guild.js
@@ -5,16 +5,15 @@ const defaultProps = {
 }
 
 const GuildInstance = class CastGuild {
-
   /**
-   * 
+   *
    * @param {Discord.Guild} guild The guild to wrap into this CastGuild
    * @param {CastGuildProperties} [properties] The properties, if any, to create this guild with.
    */
   constructor (guild, properties = defaultProps) {
     this.guild = guild
     this.properties = properties
-    this.messages = new Messages(guild.client, guild)
+    this.messages = new Messages(guild.client.config.prefix, guild.client.managers, guild)
   }
 }
 
