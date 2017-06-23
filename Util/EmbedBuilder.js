698afefb471682ab0418d4bc3f3e86b116a2fff4
Eric Rabil <EricRabil@users.noreply.github.com>
2017-06-22 22:27:03 -0400

    Made things more flexible

--- Util/EmbedBuilder.js
+++ Util/EmbedBuilder.js
@@ -1,8 +1,21 @@
 const RichEmbed = require(`discord.js`).RichEmbed
 
+const Colors = {
+  SUCCESS: 0x26FF00,
+  ERROR: 0xFF4F00
+}
+
 const EmbedDefaults = {title: null, footer: null, color: null, author: null, url: null}
 
+const version = require(`../package`).version;
+
 const EmbedBuilder = {
+
+  /**
+   * Creates a generic embed, not really a reason
+   * @param {String} text 
+   * @param {MessageEmbed} data 
+   */
   createEmbed (text, data = EmbedDefaults) {
     if (typeof data.footer === 'string') {
       data.footer = {text: data.footer}
@@ -10,7 +23,7 @@ const EmbedBuilder = {
     var embedData = {
       description: text,
       title: data.title,
-      footer: data.footer,
+      footer: {text: `Cast v${version}${data.footer ? data.footer.text ? ` - ${data.footer.text}` : '' : ''}`},
       color: data.color,
       url: data.url
     }
@@ -23,15 +36,17 @@ const EmbedBuilder = {
     return new RichEmbed(embedData)
   },
 
+
   createSuccessEmbed (text, data = EmbedDefaults) {
-    data.color = 0x26FF00
+    data.color = Colors.SUCCESS;
     return EmbedBuilder.createEmbed(text, data)
   },
 
   createErrorEmbed (text, data = EmbedDefaults) {
-    data.color = 0xFF4F00
+    data.color = Colors.ERROR;
     return EmbedBuilder.createEmbed(text, data)
   }
 }
 
 module.exports = EmbedBuilder
+module.exports.Colors = Colors;
