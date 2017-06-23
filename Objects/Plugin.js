2fb088b9dbcd602f07f27bdd498402285cf8cad0
Eric Rabil <EricRabil@users.noreply.github.com>
2017-06-23 10:00:53 -0400

    Finished the BasePlugin

--- Objects/Plugin.js
+++ Objects/Plugin.js
@@ -1,12 +1,28 @@
 const EventEmitter = require('events').EventEmitter
-const Events = require('discord.js').Constants.Events
+const fs = require(`fs-extra`)
+const path = require(`path`)
+const CommandsManager = require(`../Managers/CommandsManager`)
+const Logger = require(`../Util/Logger`)
 
 class Plugin extends EventEmitter {
-  constructor (client, metadata) {
+  constructor (client, metadata, pluginPath, resolve = null) {
     super()
     this.client = client
     this.metadata = metadata
+    this.metadata.pluginPath = pluginPath
+    this.managers = {}
+    if (metadata.commandsPath) {
+      var cmdPath = path.join(pluginPath, metadata.commandsPath)
+      if (fs.pathExistsSync(cmdPath)) {
+        var cmdMgr = new CommandsManager(this.client, cmdPath, this)
+        cmdMgr.loadCommandsSync()
+        this.managers.commands = cmdMgr
+      }
+    }
+    this.logger = new Logger('bih')
+    this.log = this.logger.log
+    this.logError = this.logger.logError
   }
 }
 
-module.exports = Plugin;
+module.exports = Plugin
