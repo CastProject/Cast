254ec77265053194bf7e76d0b310b1fe1d691121
Eric Rabil <EricRabil@users.noreply.github.com>
2017-06-26 20:30:52 -0400

    Permissions node has been finalized

--- Permissions/PermNode.js
+++ Permissions/PermNode.js
@@ -4,19 +4,43 @@ class PermNode {
    * @param {String} name The name of this node
    * @param {PermNode[]} [parent] The parent permnodes, if any
    */
-  constructor(name, parent = null) {
+  constructor(name, parents = null) {
     this.name = name;
-    this.parent = parent;
+    this.parents = parents;
   }
 
+  /** Convert the permnode into a permission string */
   serialize() {
     var builder = "";
-    this.parent.forEach(node => {
+    this.parents.forEach(node => {
       builder += `${node.name}.`
     });
     builder += this.name;
     return builder;
   }
+
+  wildcard() {
+    return this.name === '*';
+  }
+}
+
+/**
+ * Convert a permission string to a permnode
+ * @param {String} permission The permission to deserialize into nodes
+ */
+const deserialize = function(permission) {
+  var parents = [];
+  var finalNode = null;
+  var nodes = permission.split(".");
+  nodes.forEach((node, index) => {
+    var root = !nodes[index - 1];
+    var bottom = !nodes[index + 1];
+    var permNode = new PermNode(node, root ? [] : parents);
+    if (!bottom) parents.push(permNode);
+    else finalNode = permNode;
+  })
+  return finalNode;
 }
 
 module.exports = PermNode;
+module.exports.deserialize = deserialize;
