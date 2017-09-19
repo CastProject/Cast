c9c51530d5404d93a725b9cf499ee29b951f5e68
Eric Rabil <ericjrabil@gmail.com>
2017-09-19 19:41:54 -0400

    Uploaded dist files, sorry about that

--- dist/client/cast.d.ts
+++ dist/client/cast.d.ts
@@ -1,8 +1,11 @@
 import * as Discord from 'discord.js';
+import { MessageListener } from './listeners/messageListener';
 import { Logger } from './util/logger';
 export interface Cast {
     client: Discord.Client;
     logger: Logger;
+    messageListeners: Map<Discord.Snowflake, MessageListener>;
+    dmListener: MessageListener;
     isDebug(): boolean;
     getToken(): string;
     getGlobalAdmins(): string[];
--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,4 @@
 export * from './client/cast';
 export * from './client/util/util';
 export * from './client/util/logger';
+export * from './client/listeners/messageListener';
