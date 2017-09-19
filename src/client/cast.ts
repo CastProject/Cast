485ec8beca8f80d7f9e69bcf90333f900028fc29
Eric Rabil <ericjrabil@gmail.com>
2017-09-19 19:37:12 -0400

    Added messageListener property to client interface

--- src/client/cast.ts
+++ src/client/cast.ts
@@ -1,9 +1,12 @@
 import * as Discord from 'discord.js';
+import {MessageListener} from './listeners/messageListener';
 import {Logger} from './util/logger';
 
 export interface Cast {
   client: Discord.Client;
   logger: Logger;
+  messageListeners: Map<Discord.Snowflake, MessageListener>;
+  dmListener: MessageListener;
   
   isDebug(): boolean;
   getToken(): string;
