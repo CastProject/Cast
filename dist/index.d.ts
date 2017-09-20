5224d9c529256b1800b2c113c6850eefb182e92f
Eric Rabil <ericjrabil@gmail.com>
2017-09-20 19:00:57 -0400

    Export new plugin interfaces to make them publicly accessible

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -3,3 +3,6 @@ export * from './client/util/util';
 export * from './client/util/logger';
 export * from './client/listeners/messageListener';
 export * from './client/util/embedFactory';
+export * from './client/plugins/plugin';
+export * from './client/plugins/pluginConfig';
+export * from './client/containers/pluginContainer';
