265e4f357872354632524a5abcacfc3fe8539116
Eric Rabil <ericjrabil@gmail.com>
2017-09-23 13:52:05 -0400

    woops

--- dist/client/util/logger.d.ts
+++ dist/client/util/logger.d.ts
@@ -4,5 +4,5 @@ export interface Logger {
     warn(...contents: string[]): any;
     error(...contents: string[]): any;
     debug(...contents: string[]): any;
-    printError(...contents: string[]): any;
+    printError(error: Error): any;
 }
