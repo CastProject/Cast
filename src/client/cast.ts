0e8ad3518029756c4eb6c242a041f6da2427baed
Eric Rabil <ericjrabil@gmail.com>
2017-09-24 10:03:52 -0700

    Changed hasPermission to a promise due to the likelihood of a Mongoose lookup

--- src/client/cast.ts
+++ src/client/cast.ts
@@ -160,10 +160,10 @@ export interface Cast {
    * 
    * @param {(Discord.Role | Discord.GuildMember | Discord.User)} entity The entity to check against
    * @param {string} permission The permission to check for
-   * @returns {boolean} Whether they have permission
+   * @returns {Promise<boolean>} Whether they have permission
    * @memberof Cast
    */
-  hasPermission(entity: Discord.Role | Discord.GuildMember | Discord.User, permission: string): boolean;
+  hasPermission(entity: Discord.Role | Discord.GuildMember | Discord.User, permission: string): Promise<boolean>;
 
 
   /**
