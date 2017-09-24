c11f3d353e87b33481a00baa73765f6b8c3b44e3
Eric Rabil <ericjrabil@gmail.com>
2017-09-24 10:00:44 -0700

    Updated how PermissionTrackers are identified

--- dist/client/commands/permissions/permissionTracker.d.ts
+++ dist/client/commands/permissions/permissionTracker.d.ts
@@ -1,16 +1,12 @@
 import * as Discord from 'discord.js';
 import { PermissionNode } from './permissionNode';
 export interface PermissionTracker {
-    user?: Discord.Snowflake;
-    role?: Discord.Snowflake;
-    guild?: Discord.Snowflake;
+    id: Discord.Snowflake;
     hasPermission(permission: string | PermissionNode): boolean;
     addPermission(permission: string | PermissionNode): void;
     revokePermission(permission: string | PermissionNode): void;
     serialize(): {
-        user?: Discord.Snowflake;
-        role?: Discord.Snowflake;
-        guild?: Discord.Snowflake;
+        id: Discord.Snowflake;
         permissions: string[];
     };
 }
--- src/client/commands/permissions/permissionTracker.ts
+++ src/client/commands/permissions/permissionTracker.ts
@@ -2,13 +2,11 @@ import * as Discord from 'discord.js';
 import {PermissionNode} from './permissionNode';
 
 export interface PermissionTracker {
-    user?: Discord.Snowflake;
-    role?: Discord.Snowflake;
-    guild?: Discord.Snowflake;
+    id: Discord.Snowflake;
 
     hasPermission(permission: string | PermissionNode): boolean;
     addPermission(permission: string | PermissionNode): void;
     revokePermission(permission: string | PermissionNode): void;
 
-    serialize(): {user?: Discord.Snowflake, role?: Discord.Snowflake, guild?: Discord.Snowflake, permissions: string[]}
+    serialize(): {id: Discord.Snowflake, permissions: string[]}
 }
