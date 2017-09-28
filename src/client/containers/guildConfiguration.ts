e1243fe7ab16ec68f2eb32c3cc37d2926b776c95
Eric Rabil <ericjrabil@gmail.com>
2017-09-28 19:42:28 -0400

    nvm

--- src/client/containers/guildConfiguration.ts
+++ src/client/containers/guildConfiguration.ts
@@ -2,5 +2,5 @@ export interface GuildConfiguration {
   enabledPlugins: Array<string>;
   disabledPlugins: Array<string>;
   defaultPermissions: Array<string>;
-  [key: string]: Array<string | number | boolean> | string | number | boolean | Function;
+  [key: string]: Array<string | number | boolean> | string | number | boolean;
 }
