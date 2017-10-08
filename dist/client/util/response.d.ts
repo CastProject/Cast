717d7dd3a0497073b1bc38bc89d4c4516892f395
Eric Rabil <ericjrabil@gmail.com>
2017-10-08 12:10:13 -0400

    Forcing git to update dist

--- dist/client/util/response.d.ts
+++
@@ -1,7 +0,0 @@
-import { Message, RichEmbed, GuildMember, User } from "discord.js";
-export interface Response {
-    message: Message;
-    reply(content: string | RichEmbed, user?: User | GuildMember): Promise<Message>;
-    edit(content: string | RichEmbed): Promise<Message>;
-    delete(): Promise<void>;
-}
