781cdc27d9b3c35530ac81b38b2930c4681436da
Eric Rabil <ericjrabil@gmail.com>
2017-11-26 14:56:06 -0500

    Move mini-plugin spec to its own module

---
+++ src/client/plugins/miniplugin.ts
@@ -0,0 +1,47 @@
+import {Cast} from "../cast";
+import {Message} from "discord.js";
+
+export module MiniPlugin {
+  export type MiniCommandOperator = (this: Plugin, response: Response, message: Message, args: string[]) => Promise<void> | void;
+  export type MiniEventOperator = (this: Plugin) => Promise<void> | void;
+  export type StateChangeOperator = (this: Plugin) => Promise<void>;
+  
+  
+  export type MiniCommand = {
+    name: string,
+    operator: MiniCommandOperator,
+    permission?: string,
+    environments: ["text" | "dm"],
+  };
+  
+  export type MiniEvent = {
+    event: string,
+    operator: MiniEventOperator;
+  };
+  
+  export type DumpedPlugin = {
+    commands: MiniCommand[],
+    events: MiniEvent[],
+    name?: string,
+    version?: string,
+    id?: string,
+    enabled?: StateChangeOperator,
+    disabled?: StateChangeOperator,
+  };
+  
+  export type MetadataOpt = "name" | "version" | "id";
+  
+  export interface PluginBuilder {
+    cast: Cast;
+    readonly interactedWith: boolean;
+    command(name: string, operator: MiniCommandOperator, environments?: ["text", "dm"]): void;
+    on(event: string, operator: MiniEventOperator): void;
+    enabled(operator: StateChangeOperator): void;
+    disabled(operator: StateChangeOperator): void;
+    set(key: MetadataOpt, value: string): void;
+    dumpedPlugin: DumpedPlugin;
+    plugin: Plugin;
+  }
+  
+  export type MiniPlugin = (plugin: PluginBuilder) => Promise<void>;
+}
